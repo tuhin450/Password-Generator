5e92093f9bed70a83be5d31c476edd28cb15e4f2
tuhin450 <65481098+tuhin450@users.noreply.github.com>
2020-08-16 14:47:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,160 @@
+//Arrays With the Characters
+
+var uppercase = ["A", "B", "C",
+ "D", "E", "F", "G", "H", "I",
+ "J", "K", "L", "M", "N", "O",
+ "P", "Q", "R", "S", "T", "U",
+ "V", "W", "X", "Y", "Z"
+];
+
+var lowercase = ["a", "b", "c",
+ "d", "e", "f", "g", "h", "i",
+ "j", "k", "l", "m", "n", "o",
+ "p", "q", "r", "s", "t", "u",
+ "v", "w", "x", "y", "z"
+];
+
+var numbers = ["1", "2", "3", "4",
+ "5", "6", "7", "8", "9", "0"
+];
+
+var specialCharacters = ["!", "@",
+ "#", "$", "%", "&" ,"^", "*", "(", "_", ")", "-"
+];
+
+
+// A pickRandom function
+
+function pickRandom(myArray) {
+ let random = Math.floor(Math
+  .random() * myArray.length);
+
+ return myArray[random];
+}
+
+//Array of randoms
+
+var funcUpp = [
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase),
+ pickRandom(uppercase)
+];
+
+var funcLow = [
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase),
+ pickRandom(lowercase)
+];
+
+var funcNum = [
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers),
+ pickRandom(numbers)
+];
+
+
+var funcSpCh = [
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+ pickRandom(specialCharacters),
+
+];
+
+//Randoms
+
+var a = [
+ pickRandom(funcUpp),
+ pickRandom(funcLow),
+ pickRandom(funcNum),
+ pickRandom(funcSpCh),
+ pickRandom(funcUpp),
+ pickRandom(funcLow),
+ pickRandom(funcNum),
+ pickRandom(funcSpCh),
+ pickRandom(funcUpp),
+ pickRandom(funcLow),
+ pickRandom(funcNum),
+ pickRandom(funcSpCh),
+ pickRandom(funcUpp),
+ pickRandom(funcLow),
+ pickRandom(funcNum),
+ pickRandom(funcSpCh),
+ pickRandom(funcUpp),
+ pickRandom(funcLow),
+ pickRandom(funcNum),
+ pickRandom(funcSpCh),
+ pickRandom(funcUpp),
+ pickRandom(funcLow),
+ pickRandom(funcNum),
+ pickRandom(funcSpCh)
+];
+
+function generatePassword() {
+
+ var password = pickRandom(a) +
+  pickRandom(a) +
+  pickRandom(a) +
+  pickRandom(a) +
+  pickRandom(a) +
+  pickRandom(a) +
+  pickRandom(a) +
+  pickRandom(a);
+
+ console.log(password);
+ document.getElementById("pass")
+  .innerHTML = password;
+
+ document.getElementById(
+   "mensaje").innerHTML =
+  "Feel free to use these passwords!";
+}
